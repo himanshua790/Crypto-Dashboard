2a9252ade356ea2175c71743a3061a27227f1848
himanshua790 <himanshua790@gmail.com>
2021-05-14 01:18:15 +0530

    Static HomePage

--- crypto/settings.py
+++ crypto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ##New app
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'crypto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +69,10 @@ TEMPLATES = [
         },
     },
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+
 
 WSGI_APPLICATION = 'crypto.wsgi.application'
 
--- home/views.py
+++ home/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import requests
+
 
 def home(request):
-    return HttpResponse("Hello, Django!")
-    
+    data = {}
+    data["crypto_data"] = get_crypto_data()
+    return render(request, "index.html", data)
+
+
+# return the data received from api as json object
+def get_crypto_data():
+    api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=10"
+
+    try:
+        data = requests.get("https://api.coinmarketcap.com/v1/ticker/?limit=10").json()
+    except Exception as e:
+        print(e)
+        data = dict()
+
+    return data
+
+
