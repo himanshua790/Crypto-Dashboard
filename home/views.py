0b671af746330437c7291c45608b5fe9c097b21e
unknown <tiwarisaurav127@gmail.com>
2021-05-15 00:33:20 +0530

    sauravedited

---
+++ home/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import requests
+import os
+
+def home(request):
+    data = {}
+    data["crypto_data"] = get_crypto_data()
+    return render(request, "index.html", data)
+
+
+# return the data received from api as json object
+def get_crypto_data():
+    api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=10"
+
+    try:
+        data = requests.get("https://api.coinmarketcap.com/v1/ticker/?limit=10").json()
+    except Exception as e:
+        print(e)
+        data = dict()
+#saurav
+    
+    return data
+
+
