1bba7fd93e99149289846afe1bdcc4355eeed692
himanshua790 <himanshua790@gmail.com>
2021-05-22 02:39:14 +0530

    Historical data Model Created

--- home/admin.py
+++ home/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Api_data)
+admin.site.register(Crypto_data)
--- home/models.py
+++ home/models.py
@@ -2,18 +2,27 @@ from django.db import models as djmodel
 from django.db.models.base import Model
 from djongo import models
 # Create your models here.
+
+
 class Api_data(models.Model):
     crypto_id = models.CharField(max_length=50)
     rank = models.IntegerField()
     symbol = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
-    supply = models.CharField(max_length=50)
-    maxSupply = models.CharField(max_length=50)
-    marketCapUsd = models.CharField(max_length=50)
-    volumeUsd24Hr = models.CharField(max_length=50)
-    priceUsd = models.CharField(max_length=50)
-    changePercent24Hr = models.CharField(max_length=50)
-    vwap24Hr = models.CharField(max_length=50)
-    explorer = models.CharField(max_length=50)
+    supply = models.BigIntegerField()
+    maxSupply =  models.BigIntegerField()
+    marketCapUsd =  models.BigIntegerField()
+    volumeUsd24Hr =  models.BigIntegerField()
+    priceUsd = models.DecimalField(decimal_places=4,max_digits=20)
+    changePercent24Hr = models.DecimalField(decimal_places=2,max_digits=20)
+    vwap24Hr = models.DecimalField(decimal_places=2,max_digits=20)
 
 
+class Crypto_data(models.Model):
+    _id = models.ObjectIdField()
+    Date = models.DateTimeField()
+    Close = models.DecimalField(decimal_places=4,max_digits=20)
+    High = models.DecimalField(decimal_places=4,max_digits=20)
+    Low = models.DecimalField(decimal_places=4,max_digits=20)
+    Open = models.DecimalField(decimal_places=4,max_digits=20)
+    Volume = models.BigIntegerField()
