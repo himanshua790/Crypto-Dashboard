8ae4d01d719cd0e0ff9db16415aee5df048b3027
himanshua790 <himanshua790@gmail.com>
2021-05-21 14:05:37 +0530

    Dashboard Added

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
+# home/urls.py
 from django.urls import path
 from home import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("currency/<currency>",views.dashboard),
 ]
--- home/views.py
+++ home/views.py
@@ -6,6 +6,10 @@ from .models import Api_data
 #List of Crypto Required
 all_curr = ['bitcoin',"ethereum","litecoin","cardano","polkadot","dogecoin",'stellar',"chainlink","binance-coin","tether"]
 test_curr=['bitcoin']
+
+#API Url
+api_url = "https://api.coincap.io/v2/assets/"
+
 def home(request):
     data = get_crypto_data()
     print(data)
@@ -16,12 +20,9 @@ def home(request):
 # return the data received from api as json object
 def get_crypto_data():
     data_list = []
-    api_url = "https://api.coincap.io/v2/assets/"
-    
-    for curr in all_curr:
+    for currency in all_curr:
         try:
-            data = requests.get(api_url+"/"+curr).json()
-           
+            data = requests.get(api_url+"/"+currency).json()
         except Exception as e:
             data={"error":f"exception occured in get_crypto_data {e}"}
             print(e)
@@ -29,3 +30,13 @@ def get_crypto_data():
         data_list.append(data['data'])
     print("this is data list ",data_list)
     return data_list
+
+
+def dashboard(request,currency):
+    e = None
+    try:
+        data = requests.get(api_url+str(currency)).json()
+    except Exception as e:
+        data={"error":f"exception occured in get_crypto_data {e}"}
+        print(e)
+    return render(request, "dashboard.html", {'data':data,'profile':currency,'error':e})
