3ab924216b466b7da08efe2c42cd9a550c95e2f4
himanshua790 <himanshua790@gmail.com>
2021-05-22 20:58:03 +0530

    Percentage Change Added

--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
+
+import requests, datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
-import requests
-from .models import Api_data
+from .models import Api_data, Crypto_data
 from .forms import ContactForm
+from datetime import date, timedelta
+from datetime import datetime as dt
+
+def yesterday():
+    yesterday = (dt.strptime(str(dt.today().date()), '%Y-%m-%d').replace(tzinfo=datetime.timezone.utc)-datetime.timedelta(days=1)).isoformat()
+    return yesterday
+def compare(x,y):
+    return float("{:.2f}".format((x-y)/x))
 
 #List of Crypto Required
 all_curr = ['bitcoin',"ethereum","litecoin","cardano","polkadot","dogecoin",'stellar',"chainlink","binance-coin","tether"]
@@ -11,9 +21,6 @@ test_curr=['bitcoin']
 
 #API Url
 api_url = "https://api.coincap.io/v2/assets/"
-#https://api.coincap.io/v2/assets/v2/candles?exchange=poloniex&interval=h8&baseId=ethereum&quoteId=bitcoin
-
-#https://api.coincap.io/v2/assets/v2/candles?&interval=d1&baseId=ethereum&quoteId=bitcoin
 
 def home(request):
     data = get_crypto_data()
@@ -59,10 +66,20 @@ def get_crypto_data():
 
 
 def dashboard(request,currency):
-    e = None
+    e ,f = None, None
+
+    changePercent24Hr = None
     try:
         data = requests.get(api_url+str(currency)).json()
     except Exception as e:
-        data={"error":f"exception occured in get_crypto_data {e}"}
+        data={"error":f"exception occured in API Requests {e}"}
         print(e)
-    return render(request, "dashboard.html", {'data':data["data"],'profile':currency,'error':e})
+    
+    try:
+        query = Crypto_data.objects.filter(Name = str(currency),Date = yesterday()).order_by('-Date').values()[0]
+        print(f'query is:{query["Volume"]}')
+        changePercent24Hr = compare(float(data['data']['volumeUsd24Hr']),float(query['Volume']))
+    except Exception as f:
+        print(f'query except triggered for {f}')
+        query = None
+    return render(request, "dashboard.html", {'data':data["data"],'query':query,'profile':currency,'error':e,'changePercent24Hr':changePercent24Hr})
